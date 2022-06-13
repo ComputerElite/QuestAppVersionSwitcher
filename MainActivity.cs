26d3fc3b7c5a4d64ecfca2574cfb3a71389a5ddf
ComputerElite <>
2022-06-13 17:46:06 +0200

    Revert "no pls"
    This reverts commit fcefc16ab385cec87a12d724a88e7afec4ca4ac1.

--- MainActivity.cs
+++ MainActivity.cs
@@ -4,78 +4,16 @@ using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
 using Android.Webkit;
-using AndroidX.Activity.Result;
-using AndroidX.Activity.Result.Contract;
 using AndroidX.AppCompat.App;
 using AndroidX.Core.App;
 using AndroidX.Core.Content;
 using ComputerUtils.Android;
 using ComputerUtils.Android.Logging;
 using Google.Android.Material.Snackbar;
-using Java.Interop;
 using QuestAppVersionSwitcher.Core;
-using System;
 
 namespace QuestAppVersionSwitcher
 {
-    public class ActivityCallback : Java.Lang.Object, IActivityResultCallback
-    {
-        public IntPtr Handle => throw new NotImplementedException();
-
-        public int JniIdentityHashCode => throw new NotImplementedException();
-
-        public JniObjectReference PeerReference => throw new NotImplementedException();
-
-        public JniPeerMembers JniPeerMembers => throw new NotImplementedException();
-
-        public JniManagedPeerStates JniManagedPeerState => throw new NotImplementedException();
-
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Disposed()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DisposeUnlessReferenced()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Finalized()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnActivityResult(Java.Lang.Object result)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SetJniIdentityHashCode(int value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SetJniManagedPeerState(JniManagedPeerStates value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SetPeerReference(JniObjectReference reference)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UnregisterFromRuntime()
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     [Activity(Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
@@ -83,7 +21,6 @@ namespace QuestAppVersionSwitcher
         protected override void OnCreate(Bundle savedInstanceState)
         {
             Android.Webkit.WebView.SetWebContentsDebuggingEnabled(true);
-            AndroidCore.launcher = RegisterForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityCallback());
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
