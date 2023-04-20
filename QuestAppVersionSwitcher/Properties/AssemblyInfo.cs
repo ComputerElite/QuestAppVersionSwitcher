b19f967d27cacc946c848370e498e1ab8f382efe
ComputerElite <ComputerElite@CrashGods>
2023-04-20 22:46:03 +0200

    bump version

--- QuestAppVersionSwitcher/Properties/AssemblyInfo.cs
+++ QuestAppVersionSwitcher/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.12.12.0")]
-[assembly: AssemblyFileVersion("1.12.12.0")]
+[assembly: AssemblyVersion("1.12.13.0")]
+[assembly: AssemblyFileVersion("1.12.13.0")]
