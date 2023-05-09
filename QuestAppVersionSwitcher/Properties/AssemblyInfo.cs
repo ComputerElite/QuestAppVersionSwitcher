12835b75134c87c66369353ee0e2dd2c0ef6414a
ComputerElite <ComputerElite@CrashGods>
2023-05-09 16:26:38 +0200

    update version to 1.13.10

--- QuestAppVersionSwitcher/Properties/AssemblyInfo.cs
+++ QuestAppVersionSwitcher/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.13.11.0")]
-[assembly: AssemblyFileVersion("1.13.11.0")]
+[assembly: AssemblyVersion("1.13.10.0")]
+[assembly: AssemblyFileVersion("1.13.10.0")]
