dddfdcdfdf0a5272ed0beadd8b509da77cda3006
Alex Uskov <FrozenAlex@users.noreply.github.com>
2023-05-28 23:48:41 +0400

    Switch to pnpm, switch to module.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 
+import 'solid-devtools'
 
 import App from './App';
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   base: "./",
-  plugins: [solidPlugin()],
+  plugins: [
+    
+    solidPlugin(),
+    devtools({
+      autoname: true,
+      locator: {
+        targetIDE: 'vscode',
+        key: 'Ctrl',
+        jsxLocation: true,
+        componentLocation: true,
+      },
+    }),
+  ],
   server: {
     port: 3000,
     proxy: {
