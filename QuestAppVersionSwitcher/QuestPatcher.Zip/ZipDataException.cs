202a3108eeb89bcef8969a82036fe4f346dba773
ComputerElite <ComputerElite@CrashGods>
2023-10-22 15:25:34 +0200

    Add QuestPatcher.Zip and import missing types

---
+++ QuestAppVersionSwitcher/QuestPatcher.Zip/ZipDataException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuestPatcher.Zip
+{
+    /// <summary>
+    /// Thrown if the data to write to the ZIP file has a format problem
+    /// </summary>
+    public class ZipDataException : Exception
+    {
+        public ZipDataException(string message) : base(message) { }
+    }
+}
